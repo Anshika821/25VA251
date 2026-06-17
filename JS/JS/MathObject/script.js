adcaec22a99d471f06d6a3ce96d1c9b73bd4e8ed
Anshika821 <anshikac484@gmail.com>
2026-06-17 21:59:50 +0530

    mathobject

---
+++ JS/JS/MathObject/script.js
@@ -0,0 +1,12 @@
+function showMathMethods()
+{
+    let number = 5.7;
+
+    alert(
+        "Round: " + Math.round(number) +
+        "\nFloor: " + Math.floor(number) +
+        "\nCeil: " + Math.ceil(number) +
+        "\nSquare Root of 25: " + Math.sqrt(25) +
+        "\nRandom Number: " + Math.random()
+    );
+}
