e71ca4be48628607642a9bc41b48cc5ebec7ebe0
Anshika821 <anshikac484@gmail.com>
2026-06-17 20:53:22 +0530

    array

---
+++ JS/array/script.js
@@ -0,0 +1,10 @@
+function showFruits()
+{
+    let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+    alert(
+        "First Fruit: " + fruits[0] +
+        "\nSecond Fruit: " + fruits[1] +
+        "\nTotal Fruits: " + fruits.length
+    );
+}
