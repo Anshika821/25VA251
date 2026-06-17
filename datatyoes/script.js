0c558e7827922c3ccf9a3707896dba33458c9363
Anshika821 <anshikac484@gmail.com>
2026-06-17 19:32:33 +0530

    datatypes

---
+++ datatyoes/script.js
@@ -0,0 +1,16 @@
+function showDataTypes()
+{
+    let name = "Anshika";      // String
+    let age = 19;             // Number
+    let isStudent = true;     // Boolean
+    let city;                 // Undefined
+    let marks = null;         // Null
+
+    alert(
+        "Name: " + name +
+        "\nAge: " + age +
+        "\nStudent: " + isStudent +
+        "\nCity: " + city +
+        "\nMarks: " + marks
+    );
+}
