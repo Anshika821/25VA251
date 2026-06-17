6e8caeea442c955b78d28b4347cc1e41e86c082b
Anshika821 <anshikac484@gmail.com>
2026-06-17 20:27:02 +0530

    operators

---
+++ JS/operators/script.js
@@ -0,0 +1,19 @@
+function calculate()
+{
+    let a = 10;
+    let b = 5;
+
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let division = a / b;
+    let remainder = a % b;
+
+    alert(
+        "Addition = " + sum +
+        "\nSubtraction = " + difference +
+        "\nMultiplication = " + product +
+        "\nDivision = " + division +
+        "\nModulus = " + remainder
+    );
+}
