767b58fdcedba18debee2e53f63a3f72ee9801b9
Anshika821 <anshikac484@gmail.com>
2026-06-17 20:56:45 +0530

    object

---
+++ JS/JS/objects/script.js
@@ -0,0 +1,15 @@
+function showStudent()
+{
+    let student =
+    {
+        name: "Anshika",
+        age: 19,
+        city: "Ghaziabad"
+    };
+
+    alert(
+        "Name: " + student.name +
+        "\nAge: " + student.age +
+        "\nCity: " + student.city
+    );
+}
